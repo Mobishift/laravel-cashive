62fe67d06855f4a070487f84f46b9bd76277e606
Max <max@msan.cn>
2014-06-16 21:20:04 +0800

    remove dev config

--- src/config/cashive.php
+++ src/config/cashive.php
@@ -1,6 +1,5 @@
 <?php
 
-/*
 return array(
 
 	'key' => 'YOUR_CASHIVE_KEY',
@@ -10,17 +9,5 @@ return array(
     'debug' => FALSE,
     
     'debug_host' => ""
-
-);
-*/
-return array(
-
-	'key' => 'bdcb9d3075f68bf75931a3d104d2792ec66fb6c0',
-
-    'secret' => 'cb16fa09b054af337f33990c354e197babd07cf5',
-    
-    'debug' => FALSE,
-    
-    'debug_host' => "http://127.0.0.1:9000/api/"
 
 );
